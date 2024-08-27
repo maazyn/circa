b5679ef71661f2dbb40132e20da4b8f661188281
Maazin Sherif <maazinsherif99@gmail.com>
2024-08-26 23:40:15 -0400

    Add layout for auxiliary navbar

---
+++ react-vite/src/components/AuxiliaryNav/AuxiliaryNav.jsx
@@ -0,0 +1,33 @@
+// import { NavLink, useNavigate } from "react-router-dom";
+// import { useSelector } from "react-redux";
+import { WiStars } from "react-icons/wi";
+
+import "./AuxiliaryNav.css";
+
+function AuxiliaryNav() {
+  // const sessionUser = useSelector((store) => store.session.user);
+  // const navigate = useNavigate()
+  return (
+    <nav>
+      <div className="axnavContainer">
+        <div className="axnav-Left">
+          <input className="axnav-search" type="text" placeholder="Search"></input>
+          <IoFilter className="filter-icon" />
+        </div>
+
+
+        <div className="axnav-Right" >
+        {sessionUser && (
+          <div className="axnavButtons">
+            <button>Local</button>
+            <WiStars onClick={() => setScope("Sky")}/> note to self: dont forget to set up scope state management in LandingPage
+            <button>Global</button>
+          </div>
+        )}
+        </div>
+      </div>
+  </nav>
+  );
+}
+
+export default AuxiliaryNav;
