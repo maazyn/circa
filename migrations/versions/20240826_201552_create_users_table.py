25932123b56cd08cc4a88828bbda9f0648eaa42a
Maazin Sherif <maazinsherif99@gmail.com>
2024-08-27 20:15:53 -0400

    Edit migration file

--- migrations/versions/20240826_201552_create_users_table.py
+++ migrations/versions/20240826_201552_create_users_table.py
@@ -1,12 +1,16 @@
 """create users table
 
 Revision ID: 0a2ad398c169
-Revises: 
+Revises:
 Create Date: 2024-08-26 20:15:52.261522
 
 """
 from alembic import op
 import sqlalchemy as sa
+
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 
 # revision identifiers, used by Alembic.
@@ -32,6 +36,9 @@ def upgrade():
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('username')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+
     op.create_table('locations',
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('user_id', sa.Integer(), nullable=False),
@@ -49,6 +56,8 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE locations SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
