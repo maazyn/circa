540077d27973b8eae34ca82fa8575a292cf5968d
Maazin Sherif <maazinsherif99@gmail.com>
2024-09-01 19:46:46 -0400

    Save work before running git-filter-repo

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,14 +1,38 @@
 import { NavLink, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 import ProfileButton from "./ProfileButton";
 import { useSelector } from "react-redux";
 import { IoMdSettings } from "react-icons/io";
 import { VscHistory } from "react-icons/vsc";
+import { TOMORROW_API_KEY } from '../../../../x-apis/apiKeys';
 
 import "./Navigation.css";
 
 function Navigation() {
+
   const sessionUser = useSelector((store) => store.session.user);
+  const [weatherData, setWeatherData] = useState(null);
+  const [error, setError] = useState(null);
   const navigate = useNavigate()
+
+
+  const fetchWeatherData = async () => {
+    const options = { method: "GET"};
+    try {
+      const response = await fetch(`https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=${TOMORROW_API_KEY}`, options);
+      const data = response.json();
+      setWeatherData(data)
+    } catch(err) {
+      setError(err)
+      console.error(err)
+    }
+  };
+
+  useEffect(() => {
+
+    fetchWeatherData();
+  }, [])
+
   return (
     <nav className="navContainer">
       <div className="navLeft">
