f30b700ead70b2c91a5f08ebb3e0752c40da494c
Maazin Sherif <maazinsherif99@gmail.com>
2025-02-28 12:42:44 -0500

    Add automatic entry for update profile home address

--- react-vite/src/components/UpdateProfilePage/UpdateProfilePage.jsx
+++ react-vite/src/components/UpdateProfilePage/UpdateProfilePage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { thunkUpdateUserProfile, thunkDeleteUser } from "../../redux/session";
 import { useModal } from "../../context/Modal";
@@ -16,13 +16,18 @@ function UpdateProfilePage() {
         city: user.city || "",
         region: user.region || "",
         country: user.country || "",
+        lat: user.lat || 0,
+        lng: user.lng || 0,
         profile_img: user.profile_img || ""
     });
+    const [addressSuggestions, setAddressSuggestions] = useState([]);
+    const [isDropdownVisible, setDropdownVisible] = useState(false);
+    const dropdownRef = useRef(null);
     const [errors, setErrors] = useState({});
     const [deleteErrors, setDeleteErrors] = useState({});
 
 
-    const { closeModal } = useModal();
+    // const { closeModal } = useModal();
     const navigate = useNavigate()
     const handleChange = (e) => {
         setFormData({
@@ -31,28 +36,98 @@ function UpdateProfilePage() {
         });
     };
 
+    // useEffect(() => {
+    //     const handleClickOutside = (event) => {
+    //       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+    //         setDropdownVisible(false);
+    //       }
+    //     };
+
+    //     if (isDropdownVisible) {
+    //       document.addEventListener('mousedown', handleClickOutside);
+    //     } else {
+    //       document.removeEventListener('mousedown', handleClickOutside);
+    //     }
+
+    //     return () => {
+    //       document.removeEventListener('mousedown', handleClickOutside);
+    //     };
+    // }, [isDropdownVisible]);
+
+
+    const fetchAddressSuggestions = async () => {
+        const { title } = formData;
+        if (title) {
+            const encodedInput = encodeURIComponent(title);
+            try {
+                const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${encodedInput}&format=json&addressdetails=1`);
+                const data = await response.json();
+                setAddressSuggestions(data);
+                setDropdownVisible(true);
+            } catch (error) {
+                console.error("Error fetching address suggestions:", error);
+            }
+            } else {
+            setAddressSuggestions([]);
+            setDropdownVisible(false);
+        }
+    };
+
+    // const fetchCoordinatesFromAddress = async () => {
+    //     const { city, region, country } = formData;
+    //     if (city && country) {
+    //         const query = encodeURIComponent(`${city}, ${region}, ${country}`);
+    //         try {
+    //             const response = await fetch(`https://nominatim.openstreetmap.org/search?q=${query}&format=json&limit=1`);
+    //             const data = await response.json();
+    //             if (data.length > 0) {
+    //             const { lat, lon } = data[0];
+    //             setFormData((prevData) => ({
+    //                 ...prevData,
+    //                 lat: parseFloat(lat).toFixed(4),
+    //                 lng: parseFloat(lon).toFixed(4),
+    //             }));
+    //             }
+    //         } catch (error) {
+    //             console.error("Error fetching coordinates:", error);
+    //         }
+    //     }
+    // };
+
+    const handleAddressSelect = (address) => {
+        const { lat, lon, display_name, address: locationDetails } = address;
+        setFormData((prevData) => ({
+            ...prevData,
+            city: locationDetails.city || "",
+            region: locationDetails.state || "",
+            country: locationDetails.country || "",
+            lat: parseFloat(lat).toFixed(4),
+            lng: parseFloat(lon).toFixed(4),
+        }));
+        setDropdownVisible(false);
+        // setManualEntry(false);
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         const serverResponse = await dispatch(thunkUpdateUserProfile(formData))
         if (serverResponse ) {
             setErrors(serverResponse);
-        } else {
-            closeModal();
         }
     };
 
     const handleDelete = async () => {
         if (window.confirm("Are you sure you want to delete your account? This action cannot be undone.")) {
-          const errors = await dispatch(thunkDeleteUser());
-          if (errors) {
-            setDeleteErrors(errors);
-          } else {
-            closeModal();
-            navigate("/")
-          }
+            const errors = await dispatch(thunkDeleteUser());
+            if (errors) {
+                setDeleteErrors(errors);
+            } else {
+                closeModal();
+                navigate("/")
+            }
         }
-      };
+    };
 
 
 
@@ -65,6 +140,7 @@ function UpdateProfilePage() {
                 <div className="PU-name font-light">
                     <label className=" font-light">
                     First Name
+                    <span className="required-asterisk" style={{color:"red"}}> *</span>
                     <input
                     className="name-input-field font-medium"
                         type="text"
@@ -78,6 +154,7 @@ function UpdateProfilePage() {
 
                     <label className=" font-light">
                     Last Name
+                    <span className="required-asterisk" style={{color:"red"}}> *</span>
                     <input
                     className="name-input-field font-medium"
                         type="text"
@@ -93,6 +170,7 @@ function UpdateProfilePage() {
                 <div className="PU-readOnly">
                     <label id="input-label" className=" font-light">
                     Email
+                    <span className="required-asterisk" style={{color:"red"}}> *</span>
                     <input
                     className="credentials-input-field font-medium"
                         type="text"
@@ -106,6 +184,7 @@ function UpdateProfilePage() {
 
                     <label id="input-label" className=" font-light">
                     Username
+                    <span className="required-asterisk" style={{color:"red"}}> *</span>
                     <input
                     className="credentials-input-field font-medium"
                         type="text"
@@ -119,9 +198,47 @@ function UpdateProfilePage() {
                     {errors.username && <p>{errors.username}</p>}
                 </div>
 
+
+                <div className="grid grid-cols-[4fr_1fr] gap-[10px] m-auto">
+                <label className="relative font-light w-full">
+                    Where do you live? Type your city and click search:
+                    <span className="required-asterisk" style={{color:"red"}}> *</span>
+                    <input
+                    className="title-input-field font-semibold"
+                    type="text"
+                    name="title"
+                    onChange={handleChange}
+                    required
+                    />
+                    {errors.title && <p className="error-message">{errors.title}</p>}
+                    {isDropdownVisible && addressSuggestions.length > 0 && (
+                    <ul
+                        ref={dropdownRef}
+                        className="font-light absolute w-full max-h-[130px] overflow-y-auto z-10 bg-[#f7f2ff] border  shadow-[0_4px_6px_rgba(0,0,0,0.1)] list-none m-auto border-solid border-[#ccc]"
+                    >
+                        {addressSuggestions.map((suggestion, index) => (
+                        <li
+                            className="pb-[1px]"
+                            key={index}
+                            onClick={() => handleAddressSelect(suggestion)}
+                        >
+                            {suggestion.display_name}
+                        </li>
+                        ))}
+                    </ul>
+                    )}
+                </label>
+                <div className="button-box flex flex-row gap-[2px] justify-center items-center mt-[12px]">
+                    <button type="button" onClick={fetchAddressSuggestions} className="search-button w-full align-center h-auto rounded-full text-sm font-normal border box-border border-solid border-[rgba(169,169,169)] bg-white text-black hover:bg-[#5aab57ef] hover:text-white">Search</button>
+                    <button onClick={() => handleClear()} className="cancel-button w-full m-0 align-center h-auto rounded-full text-sm box-border font-normal text-black hover:bg-[#007bffef] hover:text-white hover:shadow-none">Clear</button>
+                </div>
+                </div>
+
+
                 <div className="PU-location" >
                     <label id="input-label" className=" font-light">
                     City
+                    {/* <span className="required-asterisk" style={{color:"red"}}> *</span> */}
                     <input
                     className="city-input-field font-medium"
                         type="text"
@@ -129,6 +246,7 @@ function UpdateProfilePage() {
                         value={formData.city}
                         onChange={handleChange}
                         required
+                        readOnly
                         />
                     </label>
                     {errors.city && <p>{errors.city}</p>}
@@ -141,21 +259,54 @@ function UpdateProfilePage() {
                         name="region"
                         value={formData.region}
                         onChange={handleChange}
+                        readOnly
                         />
                     </label>
                     {errors.region && <p>{errors.region}</p>}
 
                     <label id="input-label" className=" font-light">
                     Country
+                    {/* <span className="required-asterisk" style={{color:"red"}}> *</span> */}
                     <input
                     className="country-input-field font-medium"
                         type="text"
                         name="country"
                         value={formData.country}
                         onChange={handleChange}
+                        required
+                        readOnly
                         />
                     </label>
                     {errors.country && <p>{errors.country}</p>}
+
+                    <label id="input-label" className=" font-light">
+                    Latitude
+                    {/* <span className="required-asterisk" style={{color:"red"}}> *</span> */}
+                    <input
+                    className="country-input-field font-medium"
+                        type="text"
+                        name="latitude"
+                        value={formData.lat}
+                        onChange={handleChange}
+                        readOnly
+                        />
+                    </label>
+                    {errors.lat && <p>{errors.lat}</p>}
+
+                    <label id="input-label" className=" font-light">
+                    Longitude
+                    {/* <span className="required-asterisk" style={{color:"red"}}> *</span> */}
+                    <input
+                    className="country-input-field font-medium"
+                        type="text"
+                        name="longitude"
+                        value={formData.lng}
+                        onChange={handleChange}
+                        required
+                        readOnly
+                        />
+                    </label>
+                    {errors.lng && <p>{errors.lng}</p>}
                 </div>
 
                 <label className=" font-light">
@@ -166,12 +317,13 @@ function UpdateProfilePage() {
                     name="profile_img"
                     value={formData.profile_img}
                     onChange={handleChange}
+                    required
                     />
                 </label>
                 {errors.profile_img && <p>{errors.profile_img}</p>}
 
                 <div className="profile-update-buttons">
-                    <button className="submit-button" type="submit">Save Changes</button>
+                    <button className="submit-button" type="submit">Save</button>
                     <button onClick={() => navigate("/profile")} className="cancel-button">Cancel</button>
                 </div>
                 <div className="delete-button-container">
